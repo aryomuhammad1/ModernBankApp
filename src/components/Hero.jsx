67312454e18affc4d00bb13917a86f34193906f7
Aryo <aryomuhammadputra@gmail.com>
2023-03-23 13:58:45 +0700

    adding Hero and Stats section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+const Hero = () => (
+  <section id="home" className={`flex flex-col md:flex-row ${styles.paddingY}`}>
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount For{" "}
+          <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+      <div className="flex flex-row justify-between items-center w-[95%]">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+          The Next <br className="hidden sm:block" />{" "}
+          <span className="text-gradient">Generation</span>
+        </h1>
+        <div className="hidden ss:flex mr-0 md:mr-4">
+          <GetStarted />
+        </div>
+      </div>
+      <h1 className="font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
+        Payment Method.
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most
+        likely to fit your needs. We examine annual percentage rates, annual
+        fees.
+      </p>
+    </div>
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <img src={robot} alt="billing" className="w-[100%] h-[100%] z-[5]" />
+      <div className="absolute z-[0] w-[60%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[60%] h-[70%] rounded-full white__gradient bottom-[25%] right-11" />
+      <div className="absolute z-[0] w-[40%] h-[60%] right-[55%] translate-x-2/4 bottom-[18%] blue__gradient" />
+    </div>
 
+    <div className={`${styles.flexCenter} ss:hidden`}>
+      <GetStarted />
+    </div>
+  </section>
+);
 export default Hero;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { stats } from "../constants";
+import styles from "../style";
 
-const Stats = () => {
-  return <div>Stats</div>;
-};
+const Stats = () => (
+  <section
+    className={`${styles.flexCenter} ss:flex-row flex-col items-center sm:mb-20 mb-6`}
+  >
+    {stats.map((stat) => {
+      return (
+        <div
+          key={stat.id}
+          className={`flex-1 flex justify-start items-center md:flex-row flex-col p-3`}
+        >
+          <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-white mr-4">
+            {stat.value}
+          </h4>
+          <p className="font-poppins font-normalxs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] uppercase text-gradient">
+            {stat.title}
+          </p>
+        </div>
+      );
+    })}
+  </section>
+);
 
 export default Stats;
