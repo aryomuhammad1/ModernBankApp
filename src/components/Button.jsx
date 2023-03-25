52fa479cd1f6c32d26b5a4fef3874650a89fba94
Aryo <aryomuhammadputra@gmail.com>
2023-03-25 12:57:43 +0700

    removing props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "../style";
 
-const Button = (props) => {
+const Button = () => {
   return (
     <div className="font-poppins font-medium px-5 py-4 rounded-[10px] bg-blue-gradient text-primary mt-8 z-[5">
-      {props.text}
+      Get Started
     </div>
   );
 };
