4ca0c734cfcd86cc7f16b17d46c9ea5e3f3a05c7
Aryo <aryomuhammadputra@gmail.com>
2023-03-25 12:54:46 +0700

    adding cardDeal section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles, { layout } from "../style";
+import { card } from "../assets";
+import Button from "./Button";
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo} mt-14 ss:mt-0 md:max-w-[45%]`}>
+        <h2 className="font-poppins font-semibold text-[48px] leading-[76.8px] text-white">
+          Find a better card deal in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} md:max-w-[80%] w-full mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+        <Button />
+      </div>
+      <div
+        className={`${styles.flexCenter} md:justify-start w-[100%] md:w-[48%] md:pl-20`}
+      >
+        <img src={card} alt="card" className="md:w-[90%] w-[80%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
