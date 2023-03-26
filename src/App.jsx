f9640ea8783a8f7c5fc673a282dad668b3088212
Aryo <aryomuhammadputra@gmail.com>
2023-03-26 15:03:48 +0700

    -

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,8 @@ const App = () => (
         <CardDeal />
         <Testimonials />
         <Clients />
-        <CTA /> <Footer />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
